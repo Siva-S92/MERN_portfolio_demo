edd63886c47a9afac9ba28ee7b604af210ca2a47
Siva-S92 <sivagraphics4fashion@gmail.com>
2024-11-06 01:27:40 +0530

    after doing the corrections in home page, it's moving to the production

--- PORTFOLIO_VITE/client/src/static_data/constant.js
+++ PORTFOLIO_VITE/client/src/static_data/constant.js
@@ -1,8 +1,8 @@
-// export const CLIENT_URL = "https://mern-portfolio-demo-frontend.vercel.app"
-// export const SERVER_URL = "https://mern-portfolio-demo-backend.vercel.app"
+export const CLIENT_URL = "https://mern-portfolio-demo-frontend.vercel.app"
+export const SERVER_URL = "https://mern-portfolio-demo-backend.vercel.app"
 
 
 
 
-export const CLIENT_URL = "http://localhost:5173"
-export const SERVER_URL = "http://localhost:5000"
+// export const CLIENT_URL = "http://localhost:5173"
+// export const SERVER_URL = "http://localhost:5000"
