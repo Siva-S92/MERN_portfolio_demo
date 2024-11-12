39392d241360a5ac083bfee8ce4f1cda894962a5
Siva-S92 <sivagraphics4fashion@gmail.com>
2024-11-13 01:59:32 +0530

    update the resume link

--- PORTFOLIO_VITE/client/src/pages/home/Resume.jsx
+++ PORTFOLIO_VITE/client/src/pages/home/Resume.jsx
@@ -13,7 +13,7 @@ function Resume() {
           <img src="images/resumeIcon.png" alt="resume" className="h-52" />
           <p className="flex text-red-500">
             Resume Attached here...👉{" "}
-            <a href="https://drive.google.com/file/d/1iWxYy30FausVupF4o4S_M6BbCt7FUx9N/view?usp=sharing" target="_blank"><FolderArrowDownIcon className="size-6 text-blue-500 cursor-pointer" /></a>
+            <a href="https://drive.google.com/file/d/1-_5zgUYQEepvXrR3MftT-uOvUs7Wt2Fo/view?usp=sharing" target="_blank"><FolderArrowDownIcon className="size-6 text-blue-500 cursor-pointer" /></a>
           </p>
         </div>
       </div>
