dfbaf843197df145a1c80fe9a3b94b27c6c10081
Siva-S92 <sivagraphics4fashion@gmail.com>
2024-11-27 20:06:39 +0530

    update social media links and it's icons

--- PORTFOLIO_VITE/client/src/pages/home/Sider.jsx
+++ PORTFOLIO_VITE/client/src/pages/home/Sider.jsx
@@ -4,11 +4,36 @@ function Sider() {
   return (
     <div className="fixed left-0 bottom-0 px-10 sm:static">
       <div className="flex flex-col sm:flex-row sm:justify-center gap-3 items-center">
-        <a href="https://www.facebook.com/"><i className="ri-facebook-circle-line text-gray-600 text-4xl"></i></a>
-        <a href="https://www.gmail.com/"><i className="ri-mail-line text-gray-600 text-4xl"></i></a>
-        <a href="https://www.instagram.com/"><i className="ri-instagram-line text-gray-600 text-4xl"></i></a>
-        <a href="https://www.linkedin.com/"><i className="ri-linkedin-box-line text-gray-600 text-4xl"></i></a>
-        <a href="https://www.github.com/"><i className="ri-github-fill text-gray-600 text-4xl"></i></a>
+        
+        <a
+          href="https://github.com/Siva-S92"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <i className="ri-github-fill text-gray-600 text-4xl"></i>
+        </a>
+        <a
+          href="www.linkedin.com/in/siva-s-6821ba321"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <i className="ri-linkedin-box-line text-gray-600 text-4xl"></i>
+        </a>
+        <a
+          href="https://www.gmail.com/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <i className="ri-mail-line text-gray-600 text-4xl"></i>
+        </a>
+        {/* <a href="https://www.instagram.com/">
+          <i className="ri-instagram-line text-gray-600 text-4xl"></i>
+        </a> */}
+        {/* <a href="https://www.facebook.com/">
+          <i className="ri-facebook-circle-line text-gray-600 text-4xl"></i>
+        </a> */}
+        
+        
         <div className="w-[1px] h-52 bg-[#4242dd6f] sm:hidden"></div>
       </div>
     </div>
@@ -16,3 +41,5 @@ function Sider() {
 }
 
 export default Sider;
+
+
